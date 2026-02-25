08357401dcb90706eeb1b7f5c0fd5f41d2cea3e8
TigridNikhil <165287784+TigridNikhil@users.noreply.github.com>
2026-02-25 23:23:04 +0530

    Enhance database configuration with SSL support
    Added SSL options for database connection.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -16,7 +16,13 @@ const sequelize = new Sequelize(
     },
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-  }
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 );
 
 module.exports = sequelize;
