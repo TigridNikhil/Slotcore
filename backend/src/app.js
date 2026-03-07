720f5e4113627aaa7815917b871faa80d004efc4
nikhilscaria3 <nikhil117tg@gmail.com>
2026-03-08 03:04:31 +0530

    feat: initialize Express application with core middlewares, security configurations, and comprehensive API routes.

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,15 +32,16 @@ const PORT = process.env.PORT || 5000;
 
 /* ------------------ MIDDLEWARES ------------------ */
 const allowedOrigins = [
-  "https://slotcore.vercel.app",
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "http://localhost:5000",
-  "http://127.0.0.1:5173",
-  "http://127.0.0.1:5174",
-  "http://127.0.0.1:5000",
-  "https://slotcore-production.up.railway.app",
-  "https://slotcorewidget.vercel.app",
+  // "https://slotcore.vercel.app",
+  // "http://localhost:5173",
+  // "http://localhost:5174",
+  // "http://localhost:5000",
+  // "http://127.0.0.1:5173",
+  // "http://127.0.0.1:5174",
+  // "http://127.0.0.1:5000",
+  // "https://slotcore-production.up.railway.app",
+  // "https://slotcorewidget.vercel.app",
+  "*",
 ];
 
 app.use(
